c5a4ba360d30ae1d8ace400d39d519682ec766bc
rmunoz <richosojason@msn.com>
2022-11-24 10:55:48 -0500

    chore: add System import

--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,5 +1,17 @@
 import sum from "./sum";
-import "./image_viewer";
+// import "./image_viewer";
 
 const total = sum(10, 5);
 console.log(total);
+
+const button = document.createElement("button");
+button.innerText = "Click me";
+
+// code splitting using the 'System' module API
+button.onclick = () => {
+  System.import("./image_viewer").then((module) => {
+    module.default();
+  });
+};
+
+document.body.appendChild(button);
