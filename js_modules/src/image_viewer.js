8b5744ced3fb3b453601906984f3c8c9edd7bfd9
rmunoz <richosojason@msn.com>
2022-11-24 10:11:41 -0500

    chore: add images to image viewer

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,6 +1,14 @@
 import "../styles/image_viewer.css";
+import big from "../assets/big.jpg";
+import small from "../assets/small.jpg";
 
-const image = document.createElement("img");
-image.src = "https://loremflickr.com/640/360";
+const imageSmall = document.createElement("img");
+// image.src = "https://loremflickr.com/640/360";
+imageSmall.src = small;
 
-document.body.appendChild(image);
+document.body.appendChild(imageSmall);
+
+const imageBig = document.createElement("img");
+imageBig.src = big;
+
+document.body.appendChild(imageBig);
